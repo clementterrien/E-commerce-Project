fdbfe94dc67c10e758c7b3852958cef61995a200
Clement <Clement Terrien>
2020-02-09 16:51:07 +0100

    creation favoriteService / favoriteController epuré

--- ecommerce/src/Controller/FavoriteController.php
+++ ecommerce/src/Controller/FavoriteController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\FavoriteProduct;
-use App\Repository\FavoriteProductRepository;
-use App\Repository\ProductRepository;
+use App\Service\Favorite\FavoriteService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,35 +13,17 @@ class FavoriteController extends AbstractController
      */
     public function showFavoriteList()
     {
-        $favoriteList = $this->getUser()->getFavoriteList();
-        $favoriteProducts = $favoriteList->getFavoriteProducts();
-
         return $this->render('favorite/showFavorite.html.twig', [
-            'favoriteProducts' => $favoriteProducts
+            'favoriteProducts' => $this->getUser()->getFavoriteList()->getFavoriteProducts()
         ]);
     }
 
     /**
      * @Route("/addtoFavorite/{product_id}", name="favorite_add")
      */
-    public function addToFavoriteList($product_id, FavoriteProductRepository $favorite, ProductRepository $productRepo)
+    public function addToFavoriteList($product_id, FavoriteService $service)
     {
-        $favoriteList = $this->getUser()->getFavoriteList();
-        $favoriteList_id = $favoriteList->getId();
-
-        if ($favorite->findOneBy(['favoriteList' => $favoriteList_id, 'Product' => $product_id]) === null) {
-
-            $new_favorite = new FavoriteProduct;
-            $new_favorite->setFavoriteList($favoriteList);
-            $productToAdd = $productRepo->findOneBy(['id' => $product_id]);
-            dump($productToAdd);
-            $new_favorite->setProduct($productToAdd);
-
-            $em = $this->getDoctrine()->getManager();
-
-            $em->persist($new_favorite);
-            $em->flush();
-        }
+        $service->add($product_id);
 
         return $this->redirectToRoute('favorite_show');
     }
@@ -51,13 +31,9 @@ class FavoriteController extends AbstractController
     /**
      * @Route("/removeFromFavorite/{favoriteProduct_id}", name="favorite_remove")
      */
-    public function removeFromFavoriteList($favoriteProduct_id, FavoriteProductRepository $FavoriteProductRepo)
+    public function removeFromFavoriteList($favoriteProduct_id, FavoriteService $service)
     {
-        $productToRemove = $FavoriteProductRepo->findOneBy(['id' => $favoriteProduct_id]);
-        $em = $this->getDoctrine()->getManager();
-
-        $em->remove($productToRemove);
-        $em->flush();
+        $service->remove($favoriteProduct_id);
 
         return $this->redirectToRoute('favorite_show');
     }
---
+++ ecommerce/src/Service/Favorite/FavoriteService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Service\Favorite;
+
+use App\Entity\FavoriteProduct;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\FavoriteProductRepository;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class FavoriteService
+{
+
+    protected $user;
+    protected $security;
+    protected $session;
+    protected $favoriteList;
+    protected $favoriteListRepo;
+    protected $productRepo;
+    protected $em;
+
+    public function __construct(
+        SessionInterface $session,
+        Security $security,
+        FavoriteProductRepository $favoriteProductRepo,
+        ProductRepository $productRepo,
+        EntityManagerInterface $em
+    ) {
+        $this->session = $session;
+        $this->security = $security;
+        $this->user = $this->security->getUser();
+        $this->favoriteList = $this->user->getFavoriteList();
+        $this->favoriteProductRepo = $favoriteProductRepo;
+        $this->productRepo = $productRepo;
+        $this->em = $em;
+    }
+
+    public function add(int $product_id)
+    {
+        $favoriteList_id = $this->favoriteList->getId();
+
+        if ($this->favoriteProductRepo->findOneBy(['favoriteList' => $favoriteList_id, 'Product' => $product_id]) === null) {
+
+            $new_favorite = new FavoriteProduct;
+            $new_favorite->setFavoriteList($this->favoriteList);
+            $productToAdd = $this->productRepo->findOneBy(['id' => $product_id]);
+            $new_favorite->setProduct($productToAdd);
+
+            $this->em->persist($new_favorite);
+            $this->em->flush();
+        }
+    }
+
+    public function remove(int $favProduct_id)
+    {
+        $productToRemove = $this->favoriteProductRepo->findOneBy(['id' => $favProduct_id]);
+
+        $this->em->remove($productToRemove);
+        $this->em->flush();
+    }
+}
