8b2a4aa37116c6e7da13aaa4be707e5073645d06
Clement <Clement Terrien>
2020-02-13 16:02:45 +0100

    fonction like et baisse des stocks

--- ecommerce/src/Controller/OrderController.php
+++ ecommerce/src/Controller/OrderController.php
@@ -2,32 +2,77 @@
 
 namespace App\Controller;
 
+use Exception;
 use App\Entity\ConfirmedOrder;
+use App\Service\Cart\CartService;
+use App\Service\Order\OrderService;
 use App\Repository\AdressRepository;
+use App\Repository\ProductRepository;
+use App\Service\Adress\AdressService;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
     /**
-     * @Route("/order", name="order")
+     * @Route("/order", name="order_preconfirmation")
      */
-    public function index(EntityManagerInterface $em, AdressRepository $adressRepo)
+    public function preconfirmation(EntityManagerInterface $em, AdressRepository $adressRepo, SessionInterface $session, CartService $cartService, AdressService $adressService)
     {
         $user = $this->getUser();
-        $adress = $adressRepo->findOneBy(['active' => 1]);
+        $adress = $adressService->getDefaultAdress();
+        $cart = serialize($session->get('cart', []));
+        $totalPrice = $cartService->getTotalPrice();
         $order = new ConfirmedOrder;
         $order
-            ->setCart('hello')
+            ->setCart($cart)
             ->setUser($user)
             ->setCreatedAt(new \DateTime())
             ->setAdress($adress)
-            ->setTotalPrice(10);
+            ->setTotalPrice($totalPrice);
 
-        $em->persist($order);
-        $em->flush();
+        $session->set('preorder', $order);
 
-        return $this->redirectToRoute('home');
+        if ($session->get('preorder', []) !== []) {
+            return $this->redirectToRoute('order_confirmation');
+        }
+    }
+
+    /**
+     * @Route("/confirm", name="order_confirmation")
+     */
+    public function confirmation(SessionInterface $session, CartService $cartService, AdressService $adressService)
+    {
+
+        return $this->render('order/confirmorder.html.twig', [
+            'cart' => $cartService->getFullCart(),
+            'totalPrice' => $cartService->getTotalPrice(),
+            'totalQuantity' => $cartService->getTotalQuantity(),
+            'adress' => $adressService->getDefaultAdress(),
+        ]);
+    }
+
+    /**
+     * @Route("/merci", name="order_validated")
+     */
+    public function validateOrder(OrderService $orderService, CartService $cartService, SessionInterface $session)
+    {
+        $orderService->validateOrder();
+        $cartService->removeCart();
+
+        return $this->render('order/thanks.html.twig');
+    }
+
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function hello(ProductRepository $productRepo, EntityManagerInterface $em)
+    {
+        $products = $productRepo->findAll();
+
+
+        return $this->render('order/thanks.html.twig');
     }
 }
--- ecommerce/src/Service/Adress/AdressService.php
+++ ecommerce/src/Service/Adress/AdressService.php
@@ -4,6 +4,7 @@ namespace App\Service\Adress;
 
 use App\Repository\AdressRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Component\Security\Core\Security;
 
 class AdressService
@@ -26,6 +27,9 @@ class AdressService
 
     public function getDefaultAdress()
     {
+        if (count($this->adressRepo->findBy(['active' => 1])) > 1) {
+            throw new Exception("There is more than 1 default adress for this user in the DB");
+        }
         return $this->adressRepo->findOneBy(['active' => 1, 'User' => $this->user]);
     }
 }
