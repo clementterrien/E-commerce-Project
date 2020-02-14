4e1a30e4a61651044181d167fa0f9a4ed7de57cd
Clement <Clement Terrien>
2020-02-14 10:17:08 +0100

    pre-commit mailer

--- ecommerce/src/Controller/HomeController.php
+++ ecommerce/src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use App\Service\Product\ProductService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +18,16 @@ class HomeController extends AbstractController
         $productService->getTop3MostLikedProducts();
 
         return $this->render('/home/home.html.twig');
+    }
+
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function hello(ProductRepository $productRepo, EntityManagerInterface $em)
+    {
+        $mailer = sfContext::getInstance()->getMailer();
+
+
+        return $this->redirectToRoute('home');
     }
 }
