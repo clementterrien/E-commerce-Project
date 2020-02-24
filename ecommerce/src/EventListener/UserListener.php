b6d3b4c82a3fe8157c4533d7621ecbf85dfe3941
Clement <Clement Terrien>
2020-02-24 20:09:35 +0100

    Send an templated email when user registers + controller when clicking on the link to activate

--- ecommerce/src/EventListener/UserListener.php
+++ ecommerce/src/EventListener/UserListener.php
@@ -4,11 +4,18 @@ namespace Application\BackendBundle\EventListener;
 
 use App\Entity\User;
 use App\Entity\FavoriteList;
+use App\Service\Email\EmailService;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 
 
 class UserEntityListener
 {
+    protected $emailService;
+
+    public function __construct(EmailService $emailService)
+    {
+        $this->emailService = $emailService;
+    }
 
     public function prePersist(LifecycleEventArgs $args)
     {
@@ -21,6 +28,16 @@ class UserEntityListener
             $entity->setCreatedAt(new \DateTime());
             $entity->setEnabled(true);
             $entity->setConfirmationToken($token);
+        }
+    }
+
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if ($entity instanceof User) {
+            $entity = $args->getEntity();
+            $this->emailService->sendRegistrationConfirmEmail($entity);
         }
     }
 }
