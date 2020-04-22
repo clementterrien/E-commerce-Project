1a8b0e6332944c9bff44144d5a0a2a54fecc35ab
Clement <Clement Terrien>
2020-04-22 15:27:55 +0200

    first commit

--- ecommerce/src/Controller/HelloController.php
+++ ecommerce/src/Controller/HelloController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\TestType;
 use Symfony\Component\Asset\Package;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
+use Symfony\Component\HttpFoundation\Request;
 
 class HelloController extends AbstractController
 {
@@ -18,6 +22,29 @@ class HelloController extends AbstractController
     public function hello()
     {
         return $this->render('/hello/index.html.twig');
+    }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function test(Request $request)
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(TestType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                dd('ok');
+            } else {
+                $this->addFlash('failure', 'Mauvais mot de passe');
+                $this->redirectToRoute('test');
+            }
+        }
+
+        return $this->render('/test/test.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
